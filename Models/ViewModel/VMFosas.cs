09d954e4aad5a368daf5f193c47757d7705c6696
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2025-04-23 13:43:37 -0300

    se corrige el gitignore

---
+++ Models/ViewModel/VMFosas.cs
@@ -0,0 +1,8 @@
+﻿namespace CemSys.Models.ViewModel
+{
+    public class VMFosas
+    {
+        public ABMRepositoryVM<Fosa>? ABMRepositoryVM { get; set; } = new ABMRepositoryVM<Fosa>();
+        public SeccionesFosa? seccion { get; set; }
+    }
+}
