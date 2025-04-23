3fb7d324d822efcf0f756985e4bca3929d7f0e85
Tomaselle2 <169102423+Tomaselle2@users.noreply.github.com>
2025-04-22 23:07:37 -0300

    se modifico el login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CemSys.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult IniciarSesion(string correo, string clave)
+        {
+            string contra = "user";
+            string correoelectronico = "user@gmail.com";
+
+            if(correo == correoelectronico && contra == clave)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewData["Login"] = "Correo o contraseña incorrecta";
+                return View("Index");
+            }
+
+
+           
+        }
+    }
+}
